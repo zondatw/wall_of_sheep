7ea2fda61379a78c3c2d20ddc6fcdbeee39e6c5f
zondaTW <u226699@yahoo.com.tw>
2016-10-27 01:04:48 +0800

    test ajax refresh

--- django_wellofsheep/wellofsheep/urls.py
+++ django_wellofsheep/wellofsheep/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'', views.table),
+    url(r'^table/$', views.table, name = 'table'),
     url(r'^get_more_tables/$', views.get_more_tables, name = 'get_more_tables'),
+    url(r'', views.index),
 ]
--- django_wellofsheep/wellofsheep/views.py
+++ django_wellofsheep/wellofsheep/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from .models import sheeps_table
 
 # Create your views here.
+def index(request):
+    return render(request, 'wellofsheep/index.html', {})
+
 def table(request):
     sheeps = sheeps_table.objects.all()
     return render(request, 'wellofsheep/table.html', {'sheeps': sheeps})
